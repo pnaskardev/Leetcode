33ef8d74b60aeac626a03d6744b9f6897a707ea4
Priyanshu Naskar <71266237+pnaskardev@users.noreply.github.com>
2021-09-17 17:05:03 +0530

    main

--- The K Weakest Rows in a Matrix/prog.cpp
+++ The K Weakest Rows in a Matrix/prog.cpp
@@ -6,29 +6,59 @@ class Solution
     private:
     int binary(vector<int>arr)
     {
-        int start=0,end=arr.size()-1,ans=0;
+        int start=0,end=arr.size()-1,sold_idx=0,zeroes=0;
         while(start<=end)
         {
             int mid=start+(end-start)/2;
-            if(arr[mid]>=0)
+            if(arr[mid]==0)
             {
-                start=mid+1;
-            }
-            else
-            {
-                ans=arr.size()-1-mid+1;
                 end=mid-1;
             }
+            if(arr[mid]==1)
+            {
+                sold_idx=mid+1;
+                start=mid+1;
+            }
         }
-        return ans;
+        return sold_idx;
     }
 public:
     vector<int> kWeakestRows(vector<vector<int>>& mat, int k) 
     {
+        vector<pair<int,int>>res;
+        vector<int>resfinal;
+        int c=0,m=mat.size(),n=mat[0].size();
+        for(int i=0;i<m;i++)
+        {
+            res.push_back(make_pair(binary(mat[i]),i));
+        }
+        // for(auto i:res)
+        // {
+        //     cout<<i.first<<" "<<i.second<<endl;
+        //     // cout<<i<<" ";
+        // }
+        sort(res.begin(),res.end());
+        for(int i=0;i<k;i++)
+        {
+            resfinal.push_back(res[i].second);
+        }
         
+        // cout<<endl;
+        return resfinal;
     }
 };
 int main(void)
 {
-    
+    int k;
+    vector<vector<int>>mat={{1,1,0,0,0},{1,1,1,1,0},{1,0,0,0,0},{1,1,0,0,0},{1,1,1,1,1}};
+    vector<vector<int>>mat1={{1,0,0,0},{1,1,1,1},{1,0,0,0},{1,0,0,0}};
+    vector<vector<int>>mat2={{1,0},{0,0},{1,0}};
+    k=3;
+    Solution ob;
+    auto res=ob.kWeakestRows(mat2,2);
+    for(auto i:res)
+    {
+        cout<<i<<" ";
+    }
+    cout<<endl;
 }
---
+++ The K Weakest Rows in a Matrix/tempCodeRunnerFile.cpp
@@ -0,0 +1,6 @@
+for(auto i:resfinal)
+        {
+            // cout<<i.first<<" "<<i.second<<endl;
+            cout<<i<<" ";
+        }
+        cout<<endl;
