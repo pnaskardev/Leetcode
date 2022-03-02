4c8bbf4f10293916c8ab88c79143582210da9dae
pnaskardev <pnaskardev@gmail.com>
2022-03-02 18:07:30 +0530

    main

--- Is Subsequence/prog.cpp
+++ Is Subsequence/prog.cpp
@@ -6,10 +6,28 @@ class Solution
     public:
         bool isSubsequence(string s, string t) 
         {
-            
+            int n=t.size();
+            int count=0;
+            int i=0,j=0;
+            while(j<n)
+            {
+                if(s[i]==t[j])
+                {
+                    i++;
+                    count++;
+                }
+                j++;
+            }
+            if(count==s.size())
+            {
+                return true;
+            }
+            return false;
         }
 };
 int main(void)
 {
-    
+    string s = "acb", t = "ahbgdc";
+    Solution ob;
+    cout<<ob.isSubsequence(s,t)<<endl;
 }
